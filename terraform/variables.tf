bb4f830fd1921ffea363572f62905c6d91d8f4d0
Javier Mejia <javiermejia@sasquatch.local>
2022-03-13 19:39:38 -0700

    added more variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,21 @@
+variable "vpc-id" {
+  default     = "vpc-0166425ed9bb74f12"
+  description = "VPC-ID"
+}
+
 variable "region" {
   default     = "us-west-2"
   description = "Default Region"
+}
+
+variable "ingress_ports" {
+  type        = list(number)
+  description = "List if Ingress ports"
+  default     = [443,3389]
+}
+
+variable "allowed_ips" {
+  type        = list(string)
+  description = "Allowed cidr IPs"
+  default     = ["45.50.88.178/32"]
 }
