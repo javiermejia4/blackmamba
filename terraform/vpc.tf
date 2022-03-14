235b2740eb3aa95b5d184a608ae847c217838222
Javier Mejia <javiermejia@sasquatch.local>
2022-03-13 19:40:04 -0700

    fixed vpcs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.13.0"
 
   name = "blackmamba-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.20.0/24", "10.0.21.0/24", "10.0.23.0/24"]
   public_subnets  = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
 
