75944aaca9d39a53679acd18cd0d3db155ec740f
Javier Mejia <javiermejia@sasquatch.local>
2023-03-13 00:18:46 -0700

    added vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,18 @@ variable "region" {
   default     = "us-west-2"
   description = "Default Region"
 }
+
+variable "ami" {
+  default     = "ami-036e879e3a42dd7ff"
+  description = "AWS AMI"
+}
+
+variable "instance_type" {
+  default     = "t3.micro"
+  description = "EC2 Instance Type"
+}
+
+variable "spot_price" {
+  default     = "0.60"
+  description = "EC2 Spot Price"
+}
