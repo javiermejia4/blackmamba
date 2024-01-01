ced021731cb4ed1861dac7cb36de8570e6a0fd4e
Javier Mejia <javiermejia@sasquatch.local>
2024-01-01 01:02:30 -0800

    vpc

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,6 @@
 locals {
+  name = "blackmamba"
+
   common_tags = {
     owner = "Me"
     name  = "BlackMamba"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,15 +1,30 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.4.0"
 
-  name = "blackmamba-vpc"
-  cidr = "10.0.0.0/16"
+  name = local.name
+  cidr = local.vpc_cidr
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.20.0/24", "10.0.21.0/24", "10.0.23.0/24"]
-  public_subnets  = ["10.0.100.0/24"]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
 
+  single_nat_gateway = false
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
+  tags = merge(
+    {
+      Role = local.name
+    },
+    local.common_tags
+  )
 }
